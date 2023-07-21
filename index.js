573f818ef521d5269c6de78f92fdc471b8b20005
saudkhan <saudkhanbpk@gmail.com>
2023-07-21 15:37:26 +0500

    remove the socket.io

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 import router from "./routers/Router.js";
 import otpRouter from "./routers/otpRoutes.js";
-import { Server } from 'socket.io';
+// import { Server } from 'socket.io';
 import Messages from './models/chat.messages.js';
 import User from './models/userSchema.js';
 import Conversations from './models/chat.conversation.js'
-const io = new Server(8080, {
-    cors: {
-        origin: 'http://localhost:3002',
-    },
-});
+// const io = new Server(8080, {
+//     cors: {
+//         origin: 'http://localhost:3002',
+//     },
+// });
 
 
 dotenv.config();
@@ -34,48 +34,48 @@ db.once('open', function () {
 });
 app.use("/api", router, otpRouter);
 // Socket.io
-let users = [];
-io.on('connection', socket => {
-    console.log('User connected', socket.id);
-    socket.on('addUser', userId => {
-        const isUserExist = users.find(user => user.userId === userId);
-        if (!isUserExist) {
-            const user = { userId, socketId: socket.id };
-            users.push(user);
-            io.emit('getUsers', users);
-        }
-    });
+// let users = [];
+// io.on('connection', socket => {
+//     console.log('User connected', socket.id);
+//     socket.on('addUser', userId => {
+//         const isUserExist = users.find(user => user.userId === userId);
+//         if (!isUserExist) {
+//             const user = { userId, socketId: socket.id };
+//             users.push(user);
+//             io.emit('getUsers', users);
+//         }
+//     });
 
-    socket.on('sendMessage', async ({ senderId, receiverId, message, conversationId }) => {
-        const receiver = users.find(user => user.userId === receiverId);
-        const sender = users.find(user => user.userId === senderId);
-        const user = await User.findById(senderId);
-        console.log('sender :>> ', sender, receiver);
-        if (receiver) {
-            io.to(receiver.socketId).to(sender.socketId).emit('getMessage', {
-                senderId,
-                message,
-                conversationId,
-                receiverId,
-                user: { id: user._id, fullName: user.fullName, email: user.email }
-            });
-        } else {
-            io.to(sender.socketId).emit('getMessage', {
-                senderId,
-                message,
-                conversationId,
-                receiverId,
-                user: { id: user._id, fullName: user.fullName, email: user.email }
-            });
-        }
-    });
+//     socket.on('sendMessage', async ({ senderId, receiverId, message, conversationId }) => {
+//         const receiver = users.find(user => user.userId === receiverId);
+//         const sender = users.find(user => user.userId === senderId);
+//         const user = await User.findById(senderId);
+//         console.log('sender :>> ', sender, receiver);
+//         if (receiver) {
+//             io.to(receiver.socketId).to(sender.socketId).emit('getMessage', {
+//                 senderId,
+//                 message,
+//                 conversationId,
+//                 receiverId,
+//                 user: { id: user._id, fullName: user.fullName, email: user.email }
+//             });
+//         } else {
+//             io.to(sender.socketId).emit('getMessage', {
+//                 senderId,
+//                 message,
+//                 conversationId,
+//                 receiverId,
+//                 user: { id: user._id, fullName: user.fullName, email: user.email }
+//             });
+//         }
+//     });
 
-    socket.on('disconnect', () => {
-        users = users.filter(user => user.socketId !== socket.id);
-        io.emit('getUsers', users);
-    });
-    // io.emit('getUsers', socket.userId);
-});
+//     socket.on('disconnect', () => {
+//         users = users.filter(user => user.socketId !== socket.id);
+//         io.emit('getUsers', users);
+//     });
+//     // io.emit('getUsers', socket.userId);
+// });
 app.listen(process.env.PORT || 4000, () => {
     console.log(`Listening on port ${process.env.PORT || 4000}`);
 
